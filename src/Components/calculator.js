d08973d317ecc10865fcff0c2451fff4944bdb6a
KanoCode <kanokanombola@gmail.com>
2022-05-28 02:06:41 +0200

    Refactor calculator.js, Button.js using functional components and hooks

--- src/Components/calculator.js
+++ src/Components/calculator.js
@@ -1,69 +1,59 @@
 import './calculator.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import calculate from './logic/calculate';
-// import operate from './logic/operate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      operation: null,
-      next: null,
-    };
-    this.updateCalc = this.updateCalc.bind(this);
-  }
+function Calculator() {
+  const [Data, setData] = useState({ total: 0, next: null, operation: null });
+  const updateCalc = (btnVal) => {
+    const { total, operation, next } = calculate(Data, btnVal);
+    setData({ total, operation, next });
+  };
+  const { total, operation, next } = Data;
 
-  updateCalc(btnVal) {
-    this.setState((state) => calculate(state, btnVal));
-  }
+  return (
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <ul className="container">
-        <li className="firstRow">
+    <ul className="container">
+      <li className="firstRow">
+        {' '}
+        <p>
+          {operation === 'A/C' ? null : operation}
           {' '}
-          <p>
-            {operation === 'A/C' ? null : operation}
-            {' '}
-            {next}
-          </p>
-          <p className="result">{total === null ? 0 : total}</p>
-        </li>
-        <li className="secondRow">
-          <Button text="A/C" updateCalc={this.updateCalc} />
-          <Button text="+/-" updateCalc={this.updateCalc} />
-          <Button text="%" updateCalc={this.updateCalc} />
-          <Button text="÷" updateCalc={this.updateCalc} />
-        </li>
-        <li className="thirdRow">
-          <Button text="7" updateCalc={this.updateCalc} />
-          <Button text="8" updateCalc={this.updateCalc} />
-          <Button text="9" updateCalc={this.updateCalc} />
-          <Button text="x" updateCalc={this.updateCalc} />
-        </li>
-        <li className="fourthRow">
-          <Button text="4" updateCalc={this.updateCalc} />
-          <Button text="5" updateCalc={this.updateCalc} />
-          <Button text="6" updateCalc={this.updateCalc} />
-          <Button text="-" updateCalc={this.updateCalc} />
-        </li>
-        <li className="fifthRow">
-          <Button text="1" updateCalc={this.updateCalc} />
-          <Button text="2" updateCalc={this.updateCalc} />
-          <Button text="3" updateCalc={this.updateCalc} />
-          <Button text="+" updateCalc={this.updateCalc} />
-        </li>
-        <li className="lastRow">
-          <Button text="0" updateCalc={this.updateCalc} />
-          <Button text="." updateCalc={this.updateCalc} />
-          <Button text="=" updateCalc={this.updateCalc} />
-        </li>
-      </ul>
-    );
-  }
+          {next}
+        </p>
+        <p className="result">{total === null ? 0 : total}</p>
+      </li>
+      <li className="secondRow">
+        <Button text="A/C" updateCalc={updateCalc} />
+        <Button text="+/-" updateCalc={updateCalc} />
+        <Button text="%" updateCalc={updateCalc} />
+        <Button text="÷" updateCalc={updateCalc} />
+      </li>
+      <li className="thirdRow">
+        <Button text="7" updateCalc={updateCalc} />
+        <Button text="8" updateCalc={updateCalc} />
+        <Button text="9" updateCalc={updateCalc} />
+        <Button text="x" updateCalc={updateCalc} />
+      </li>
+      <li className="fourthRow">
+        <Button text="4" updateCalc={updateCalc} />
+        <Button text="5" updateCalc={updateCalc} />
+        <Button text="6" updateCalc={updateCalc} />
+        <Button text="-" updateCalc={updateCalc} />
+      </li>
+      <li className="fifthRow">
+        <Button text="1" updateCalc={updateCalc} />
+        <Button text="2" updateCalc={updateCalc} />
+        <Button text="3" updateCalc={updateCalc} />
+        <Button text="+" updateCalc={updateCalc} />
+      </li>
+      <li className="lastRow">
+        <Button text="0" updateCalc={updateCalc} />
+        <Button text="." updateCalc={updateCalc} />
+        <Button text="=" updateCalc={updateCalc} />
+      </li>
+    </ul>
+  );
 }
 
 export default Calculator;
