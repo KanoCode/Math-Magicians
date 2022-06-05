211b50707b51451cf196a3ba8fdf9c0b8b73e407
KanoCode <kanokanombola@gmail.com>
2022-06-06 00:05:39 +0200

    Add humburger menu, make mobile responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const StyledNav = styled.nav`
   height: 60px;
@@ -15,6 +16,11 @@ const StyledNav = styled.nav`
     justify-content: space-evenly;
     align-items: center;
   }
+
+  //button 
+  button{
+    display:none;
+  }
   a {
     width: 20%;
     text-align: center;
@@ -24,20 +30,58 @@ const StyledNav = styled.nav`
     cursor: pointer;
     background: linear-gradient(45deg, #e6e6e6, #ffffff);
   }
+  @media only screen and (max-width: 768px) {
+    /* For mobile phones: */
+    div {
+      display: none;
+    }
+    button {
+      z-index: 2;
+      display:block;
+      border: none;
+      cursor: pointer;
+      width: 30%;
+      background: transparent;
+      font-size: 24px;
+    }
+    .active {
+      display: flex;
+      flex-direction: column;
+      position: fixed;
+      width: 90%;
+      top: 3px;
+      z-index: 1;
+      background: #e3e3e3;
+      bottom: 66px;
+      box-shadow: 35px 35px 70px #8a8a8a, -35px -35px 70px #c2c2c2;
+    }
+  }
 `;
-const NavBar = () => (
-  <>
-    {' '}
-    <StyledNav>
-      <h1>𝘔𝘢𝘵𝘩 𝘔𝘢𝘨𝘪𝘤𝘪𝘢𝘯𝘴</h1>
-      <div>
-        {' '}
-        <Link to="/Home">Home</Link>
-        <Link to="/calculator">Calculator</Link>
-        <Link to="/quote">Quote</Link>
-      </div>
-    </StyledNav>
-  </>
-);
+const NavBar = () => {
+  const [NavIsActive, toggleActiveNav] = useState(false);
+  return (
+    <>
+      {' '}
+      <StyledNav>
+        <h1>𝘔𝘢𝘵𝘩 𝘔𝘢𝘨𝘪𝘤𝘪𝘢𝘯𝘴</h1>
+        <div className={NavIsActive ? 'active' : ''}>
+          {' '}
+          <Link onClick={() => toggleActiveNav(!NavIsActive)} to="/Home">
+            Home
+          </Link>
+          <Link onClick={() => toggleActiveNav(!NavIsActive)} to="/calculator">
+            Calculator
+          </Link>
+          <Link onClick={() => toggleActiveNav(!NavIsActive)} to="/quote">
+            Quote
+          </Link>
+        </div>
+        <button onClick={() => toggleActiveNav(!NavIsActive)} type="button">
+          {NavIsActive ? <FaTimes /> : <FaBars />}
+        </button>
+      </StyledNav>
+    </>
+  );
+};
 
 export default NavBar;
